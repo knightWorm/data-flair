f3fa63059e8552dc659f1822b75a3b1aec775c8b
Ronal Portillo arriola <ronalportilloarriola@Ronals-MBP.lan>
2020-12-31 22:23:01 -0800

    First commit to the repo

---
+++ hello.py
@@ -0,0 +1,8 @@
+#this is the first program menat to display hello, world! for the repo
+
+def display_message():
+	print("\nHello, World!\n")
+
+#driver
+if __name__ == '__main__':
+	display_message()
